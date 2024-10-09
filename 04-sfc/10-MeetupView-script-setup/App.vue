6400d5112bd30ce736b7f198e8e2e74393f3ec87
Stanislav Ovseychuk <ziesel777@gmail.com>
2024-10-09 22:01:16 +0500

    04-sfc/10-MeetupView-script-setup

--- 04-sfc/10-MeetupView-script-setup/App.vue
+++ 04-sfc/10-MeetupView-script-setup/App.vue
@@ -7,13 +7,14 @@ import meetups from './meetups.fixture.ts'
 const meetupIndex = ref(0)
 const meetup = computed(() => meetups[meetupIndex.value])
 const options = meetups.map((_, index) => ({ label: index + 1, value: index }))
+
 </script>
 
 <template>
-  <div style="display: flex; justify-content: center; gap: 1em; padding: 1em 0">
-    <UiRadioGroup v-model="meetupIndex" :options="options" />
-  </div>
-  <div class="page-meetup">
-    <MeetupView :meetup="meetup" />
-  </div>
+	<div style="display: flex; justify-content: center; gap: 1em; padding: 1em 0">
+		<UiRadioGroup v-model="meetupIndex" :options="options" />
+	</div>
+	<div class="page-meetup">
+		<MeetupView :meetup />
+	</div>
 </template>
